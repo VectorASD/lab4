b902f30674ebbfb2d162f33cd5deb6cddbfb2adb
VectorASD <60642904+VectorASD@users.noreply.github.com>
2023-03-11 00:02:43 +0700

    Простой финальный fix'ик

--- Notepandus/Views/MainWindow.axaml.cs
+++ Notepandus/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using HarfBuzzSharp;
 using Notepandus.ViewModels;
 
 namespace Notepandus.Views {
@@ -12,7 +13,12 @@ namespace Notepandus.Views {
         private void DoubleTap(object sender, RoutedEventArgs e) {
             var mwvm = (MainWindowViewModel?) DataContext;
             if (mwvm == null) return;
-            mwvm.DoubleTap();
+
+            var src = e.Source;
+            if (src == null) return;
+
+            var name = src.GetType().Name; // припоминается питоновский obj.__type__.__name__ XD
+            if (name == "Image" || name == "ContentPresenter" || name == "TextBlock") mwvm.DoubleTap();
         }
     }
 }
